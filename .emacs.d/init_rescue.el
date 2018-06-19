9bc1c52336d4feea86fe72b31fe29f3f790a65f6
zackn9ne <zackn9ne@gmail.com>
2018-06-19 17:06:31 -0400

    Create init_rescue.el

---
+++ .emacs.d/init_rescue.el
@@ -0,0 +1,18 @@
+; no backups
+                                                                                    ;    │····························································
+(setq make-backup-files nil) ; stop creating backup~ files                          ;    │····························································
+(setq auto-save-default nil) ; stop creating #autosave# files                       ;    │····························································
+
+;
+(global-hl-line-mode +1)
+
+(setq enable-recursive-minibuffers t)
+
+    
+; random # function    
+(random t) ; seed it randomly                                                          
+                                                                                       
+(defun insert-random-number ()                                                         
+  "Insert a random number between 0 to 999999."                                        
+  (interactive)                                                                        
+  (insert (number-to-string (random 999999))) )
